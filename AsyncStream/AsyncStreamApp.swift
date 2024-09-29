fadd284c907ff6f75a21766e406e23f2d906c61d
Kevin Montemayor <kmontemayor@advancedmd.com>
2024-09-29 10:28:08 -0600

    Initial Commit

---
+++ AsyncStream/AsyncStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncStreamApp.swift
+//  AsyncStream
+//
+//  Created by Kevin Montemayor on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncStream/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AsyncStream
+//
+//  Created by Kevin Montemayor on 9/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
